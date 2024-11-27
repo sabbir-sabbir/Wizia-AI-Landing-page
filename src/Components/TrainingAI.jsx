fe75682105ffa06cd6a370b20ea637ee739f1ca3
sabbir-sabbir <iosabbir7890@gmail.com>
2024-11-27 17:30:05 +0600

    trainig ai commit

--- src/Components/TrainingAI.jsx
+++ src/Components/TrainingAI.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const TrainingAI = () => {
   return (
-    <div>
+    <>
+     <div className='w-full h-[564px] bg-[url("bg2.svg")] bg-cover bg-no-repeat '>
+        <div className='pt-24 leading-tight'>
+            <h2 className='text-[40px] text-white text-center font-semibold '>Train your aiDR on your...</h2>
+            <h2 className='text-[40px] text-btncolor italic text-center'>prefered email st|</h2>
+            <div className='w-[450px] ml-[30%] leading-tight'>
+                <p className='text-[24px] pt-6 text-white/50 text-center'>Youre in control. Train your aiDR on elements of your Marketing strategy.</p>
+            </div>
+            
+        </div>
         
     </div>
+    </>
+   
   )
 }
 
